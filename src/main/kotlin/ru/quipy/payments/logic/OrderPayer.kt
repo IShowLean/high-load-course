620b1a90a83c8ed805b104bff85b345a36eccf28
George Maximov <rhyswayne2601@gmail.com>
2025-10-26 18:25:29 +0300

    chore: add extra comments on test queue and intake rps

--- src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
+++ src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
@@ -27,9 +27,9 @@ class OrderPayer(
     @Autowired
     private lateinit var paymentService: PaymentService
 
-    // Тест 2: 11 rps, 100 - 3 мин. 30+ сек.); Тест 3: 100 rps, 5 мин.
-    private val intakeRateLimitPerSec = 11
-    private val queueCapacity = 100
+    // Тест 2: 11 rps, 100 - 3 мин. 30+ сек.); Тест 3: 100 rps, 300 - 5 мин.
+    private val intakeRateLimitPerSec = 100
+    private val queueCapacity = 300
     private val paymentQueue = LinkedBlockingQueue<Runnable>(queueCapacity)
 
     private val paymentExecutor = ThreadPoolExecutor(
